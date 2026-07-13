4ccc6abb8c34353ce92b579baeb0649190aa1a33
HimeshVijay <himeshvijay89@gmail.com>
2026-07-13 17:03:48 +0530

    added custom messages and updated heartbeat node to send custom message

--- src/shadow_diagnostics/src/heartbeat_node.cpp
+++ src/shadow_diagnostics/src/heartbeat_node.cpp
@@ -1,41 +1,59 @@
 #include <rclcpp/rclcpp.hpp>
-#include <std_msgs/msg/string.hpp>
+#include "shadow_interfaces/msg/robot_status.hpp"
 #include <chrono>
 #include <memory>
 #include <functional>
-
+#include <std_msgs/msg/string.hpp>
 
 
 class HeartbeatNode: public rclcpp::Node
 {
 private:
-        rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-        rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscriber_;
+        rclcpp::Publisher<shadow_interfaces::msg::RobotStatus>::SharedPtr status_publisher_;
+        rclcpp::Subscription<std_msgs::msg::String>::SharedPtr command_subscriber_;
         rclcpp::TimerBase::SharedPtr timer_;
-        std::string current_state_ = "running";
+        // std::string current_state_ = "running";
+
+        shadow_interfaces::msg::RobotStatus current_status_;
+        // float current_battery_percentage = 100.0;
+        // bool current_emergency_stop = false;
+        // uint16_t current_error_code = 0;
+        // float current_system_temperature = 35.0;
 
         void heartbeat_timer_callback(){
           //  RCLCPP_INFO(this->get_logger(), "Callback Executed");
-            std_msgs::msg::String msg;
-            msg.data = current_state_;
-            publisher_ -> publish(msg);
+            //shadow_interfaces::msg::RobotStatus msg;
+            // msg.battery_percentage = current_battery_percentage;    = we will use a single
+            // msg.state = current_state_;                               status publisher instead of creating five. 
+            // msg.emergency_stop = current_emergency_stop;                
+            // msg.error_code = current_error_code;                        
+            // msg.system_temperature = current_system_temperature;
+            // status_publisher_ -> publish(msg);
+            status_publisher_ ->publish(current_status_);
         }
 
         void robot_command_callback(const std_msgs::msg::String::SharedPtr msg){
-            current_state_ = msg->data;
+            current_status_.state = msg->data;
 
             RCLCPP_INFO(
                 this->get_logger(),
                 "received command: %s",
-                current_state_.c_str()
+                current_status_.state.c_str()
             );
         }
 
 public:
         HeartbeatNode(): Node("heartbeat_node"){
+
+            current_status_.state = "RUNNING";
+            current_status_.battery_percentage = 100.0f;
+            current_status_.emergency_stop = false;
+            current_status_.error_code = 0;
+            current_status_.system_temperature = 35.0f;
+
             //RCLCPP_INFO(this->get_logger(), "Heartbeat Node Started");
-            publisher_ = this-> create_publisher<std_msgs::msg::String>("/robot_status",10);
-            subscriber_= this-> create_subscription<std_msgs::msg::String>("/robot_command",
+            status_publisher_ = this-> create_publisher<shadow_interfaces::msg::RobotStatus>("/robot_status",10);
+            command_subscriber_= this-> create_subscription<std_msgs::msg::String>("/robot_command",
             10,
             std::bind(&HeartbeatNode::robot_command_callback, this, std::placeholders::_1)
             );
